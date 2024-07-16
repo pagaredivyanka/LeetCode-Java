451b942d1ee14d8e65175ce65f944703b310ce14
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-07-16 12:28:33 +0530

    726. Number of Atoms

--- Number-of-Atoms.java
+++ Number-of-Atoms.java
@@ -1,75 +1,80 @@
+class Solution {
+    private boolean isNum(int i, String s) {
+        return s.charAt(i) >= '0' && s.charAt(i) <= '9';
+    }
 
-from collections import defaultdict
-from collections import deque
-import re
+    private boolean isLower(int i, String s) {
+        return s.charAt(i) >= 'a' && s.charAt(i) <='z';
+    }
 
-class Solution:
-    def isNum(self, i, s):
-        return '0' <= s[i] <= '9'
+    public String countOfAtoms(String formula) {
+        Stack<String> stack = new Stack<>();
+        for(int i = 0; i < formula.length(); i++) {
+            if(formula.charAt(i) >= 'A' && formula.charAt(i) <= 'Z') {
+                int j = i + 1;
+                while(j < formula.length() && isLower(j, formula)) {
+                    j++;
+                }
+                String curr = formula.substring(i, j);
+                stack.push(curr);
+                i=j;
+                while(j < formula.length() && isNum(j, formula)) {
+                    j++;
+                }
+                String num = formula.substring(i, j);
+                if(num.length() > 0) {
+                    stack.push(num);
+                } else {
+                    stack.push("1");
+                }
+                i = j-1;
+            }
 
-    def isLower(self, i, s):
-        return 'a' <= s[i] <= 'z'
+            else if(formula.charAt(i)=='(') {
+                stack.push(String.valueOf(formula.charAt(i)));
 
-    def countOfAtoms(self, formula: str) -> str:
-        stack = deque()
-        i = 0
-        while i < len(formula):
-            if 'A' <= formula[i] <= 'Z':
-                j = i + 1
-                while j < len(formula) and self.isLower(j, formula):
-                    j += 1
-                curr = formula[i:j]
-                stack.append(curr)
-                i = j
-                while j < len(formula) and self.isNum(j, formula):
-                    j += 1
-                num = formula[i:j]
-                if len(num) > 0:
-                    stack.append(num)
-                else:
-                    stack.append("1")
-                i = j - 1
-            elif formula[i] == '(':
-                stack.append(formula[i])
-            else:
-                j = i + 1
-                while j < len(formula) and self.isNum(j, formula):
-                    j += 1
-                num = formula[i+1:j]
-                number = 1
-                if len(num) > 0:
-                    number = int(num)
-                temp = deque()
-                while stack[-1] != '(':
-                    temp.append(stack.pop())
-                stack.pop()
-                while temp:
-                    atom = temp.pop()
-                    stack.append(atom)
-                    curr_num = int(temp.pop())
-                    stack.append(str(number * curr_num))
-                i = j - 1
-            i += 1
-        
-        atom_counts = defaultdict(int)
-        while stack:
-            num = 1
-            if stack[-1].isdigit():
-                num = int(stack.pop())
-            atom = stack.pop()
-            atom_counts[atom] += num
-        
-        result = []
-        for atom in sorted(atom_counts.keys()):
-            result.append(atom)
-            if atom_counts[atom] != 1:
-                result.append(str(atom_counts[atom]))
-        
-        return ''.join(result)
+            } else {
+                int j = i+1;
+                while(j < formula.length() && isNum(j, formula)) {
+                    j++;
+                }
+                String num = formula.substring(i+1, j);
+                int number = 1;
+                if(num.length() > 0) {
+                    number = Integer.parseInt(num);
 
-# Example usage
-sol = Solution()
-print(sol.countOfAtoms("H2O"))  # Output: "H2O"
-print(sol.countOfAtoms("Mg(OH)2"))  # Output: "H2MgO2"
+                } 
+                Stack<String> temp = new Stack<>();
+                while(!stack.peek().equals("(")) {
+                    temp.add(stack.pop());
+                }
+                stack.pop();
+                while(!temp.isEmpty()) {
+                    String atom = temp.pop();
+                    stack.push(atom);
+                    int currNum = Integer.parseInt(temp.pop());
+                    stack.push(String.valueOf(number*currNum));
+                }
+                i = j-1;
+            }
+        }    
 
-  
+        TreeMap<String, Integer> map = new TreeMap<>();
+        while(!stack.isEmpty()) {
+            int num = 1;
+            if(isNum(0, stack.peek())) {
+                num = Integer.parseInt(stack.pop());
+            }
+            String atom = stack.pop();
+            map.put(atom, map.getOrDefault(atom, 0) + num);
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for(String key: map.keySet()) {
+            ans.append(key);
+            if(map.get(key)!=1)
+                ans.append(String.valueOf(map.get(key)));
+        }
+        return ans.toString();
+    }
+}
