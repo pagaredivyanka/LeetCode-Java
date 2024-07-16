8a97b5dcba7d32aff9e9bf10192600ca0a4ec0d6
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-07-16 12:07:14 +0530

    1717. Maximum Score From Removing Substrings

---
+++ Maximum-Score-From-Removing-Substrings.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+class Solution {
+
+    public String removeSubstring(String s, String str, int points, int[] ans) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if ((s.charAt(i) == str.charAt(1)) && (!st.isEmpty()) && (st.peek() == str.charAt(0))) {
+                st.pop();
+                ans[0] += points;
+            } else {
+                st.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+
+    public int maximumGain(String s, int x, int y) {
+    int[] ans = new int[1];
+        if(x<y) {
+            s = removeSubstring(s, "ba", y, ans);
+            s = removeSubstring(s, "ab", x, ans);
+        } else {
+            s = removeSubstring(s, "ab", x, ans);
+            s = removeSubstring(s, "ba", y, ans);
+        }
+        return ans[0];
+    }
+}
